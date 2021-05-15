2f9e8d62dd95ab201da574096ee9ae8ccf7ea973
Ankita Khawle <ankita.k@zebpay.com>
2021-05-15 19:22:13 +0530

    added art block with zoom

--- src/components/molecules/ArtBlock/index.js
+++ src/components/molecules/ArtBlock/index.js
@@ -2,17 +2,22 @@ import React, {useEffect, useState } from 'react';
 import { Image} from 'antd';
 import sample from "../../../assets/samples/sample_pizza.jpg"
 
+import InnerImageZoom from 'react-inner-image-zoom';
 
 
 const Art = (props) => {
     return (
-       <div>
-            {/* <Image
-                style={{ width: 'inherit', marginBottom: '20px' }}
-                src={sample}
-                width={472}
-                height={472}
-            />	 */}
+       <div >
+           <InnerImageZoom  
+                src={sample} 
+                width={'500px'}
+                zoomSrc={sample} 
+                // fullscreenOnMobile={true}
+                // zoomScale={0.9}
+                zoomType={'hover'}
+                moveType={'drag'}
+            // zoomSrc='/path/to/zoom-image.jpg'
+             />
        </div>
     )
 }
