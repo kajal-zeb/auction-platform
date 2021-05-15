5cdae4eb6b34538623ae094fa176bb368399757d
kajal-zeb <kajal.d@zebpay.com>
2021-05-15 15:25:06 +0530

    socket example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import SocketTest from './socket';
 
 const App = () => {
-  return (
-    <div>
-      this works!
-    </div>
-  );
+	return (
+		<>
+			<div>
+				<p>Welcome to Bitcoin Pizza...</p>
+			</div>
+			<SocketTest />
+		</>
+	);
 };
 
 export default App;
