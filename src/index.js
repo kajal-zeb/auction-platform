30067ea2ffe6a128f41724c1c2ce28cd2586f534
kajal-zeb <kajal.d@zebpay.com>
2021-05-15 15:35:38 +0530

    fix render DOM issue

--- src/index.js
+++ src/index.js
@@ -4,23 +4,11 @@ import './styles/app.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { isMobileOnly } from 'react-device-detect';
+let app = document.getElementById('root');
 
-if (isMobileOnly) {
-  ReactDOM.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <div>only supports mobile view!</div>
-    </React.StrictMode>
-  )
-}
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root'),
+);
