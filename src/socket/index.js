40dc9376b7b0e738bc1c703ec24c9717135aa03c
kajal-zeb <kajal.d@zebpay.com>
2021-05-16 22:31:38 +0530

    socket example

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,22 +1,24 @@
-import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
-const ENDPOINT = "http://127.0.0.1:3001";
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const io = require('socket.io-client');
+const ENDPOINT = 'http://localhost:3003';
 const SocketTest = () => {
-  const [response, setResponse] = useState("");
+	const [response, setResponse] = useState('');
 
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("CurrentTime", data => {
-      setResponse(data);
-    });
-  }, []);
+	useEffect(() => {
+		const socket = io('http://localhost:3003');
+		socket.on('CurrentDate', (data) => {
+			console.log('Data > ', data);
+			setResponse(data);
+		});
+    return () => socket.disconnect();
+	}, []);
 
-  return (
-    <p>
-      <time dateTime={response}>{response}</time>
-    </p>
-  );
-}
+	return (
+		<p>
+			<time dateTime={response}>{response}</time>
+		</p>
+	);
+};
 
 export default SocketTest;
