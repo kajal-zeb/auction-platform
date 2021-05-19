1b24f26e41c1fcc5bfea6714f9d452ac5cc1d49d
karan-parsnani-zeb <karan.p@zebpay.com>
2021-05-19 19:33:49 +0530

    adding logo and message while setting up

--- src/components/atoms/Text/Text.js
+++ src/components/atoms/Text/Text.js
@@ -1,36 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './text.module.scss';
-const Text = ({spacing,size,italics,ellipsis,weight,children,theme,align,capitalize,noMargin,...rest}) => {
-    return (
-        <p
-            {...rest}
-            className={`${classes.text} ${spacing ? classes[spacing] : ' '} 
-            ${size ? `${classes['font'+size]}` : ` ${classes.fontsm}`}
-            ${theme ?
-                classes[theme]:'dark'} ${align ? classes[align] : ''} ${
-                capitalize ? classes.capitalize : ''
-            } ${italics? classes.italics.toString():''} ${ellipsis? classes.ellipsis.toString():''} `}
-            style={{ fontWeight: weight, margin: noMargin ? '0' : '' }} >
-            {children}
-        </p>
-    );
+const Text = ({
+  spacing,
+  size,
+  italics,
+  ellipsis,
+  weight,
+  children,
+  theme,
+  align,
+  capitalize,
+  noMargin,
+  primaryColor,
+  ...rest
+}) => {
+  return (
+    <p
+      {...rest}
+      className={`${classes.text} ${spacing ? classes[spacing] : ' '} 
+            ${size ? `${classes['font' + size]}` : ` ${classes.fontsm}`}
+            ${theme ? classes[theme] : 'dark'} ${align ? classes[align] : ''} ${
+        capitalize ? classes.capitalize : ''
+      } ${italics ? classes.italics.toString() : ''} ${
+        ellipsis ? classes.ellipsis.toString() : ''
+      } `}
+      style={{
+        fontWeight: weight,
+        margin: noMargin ? '0' : '',
+        color: primaryColor ? 'var(--primary-color)' : '',
+      }}
+    >
+      {children}
+    </p>
+  );
 };
 
 Text.defaultProps = {
-    children: '',
+  children: '',
 };
 
 Text.propTypes = {
-    children: PropTypes.any.isRequired,
-    spacing: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl', 'none']),
-    align: PropTypes.oneOf(['left', 'right', 'center']),
-    theme: PropTypes.oneOf(['red', 'light', 'dark','gray','green','lightgray']),
-    size: PropTypes.oneOf(['xs','sm', 'md', 'lg']),
-    italics: PropTypes.bool,
-    capitalize: PropTypes.bool,
-    weight: PropTypes.number,
-    ellipsis: PropTypes.bool
+  children: PropTypes.any.isRequired,
+  spacing: PropTypes.oneOf(['xs', 'sm', 'md', 'lg', 'xl', 'none']),
+  align: PropTypes.oneOf(['left', 'right', 'center']),
+  theme: PropTypes.oneOf([
+    'red',
+    'light',
+    'dark',
+    'gray',
+    'green',
+    'lightgray',
+  ]),
+  size: PropTypes.oneOf(['xs', 'sm', 'md', 'lg']),
+  italics: PropTypes.bool,
+  capitalize: PropTypes.bool,
+  weight: PropTypes.number,
+  ellipsis: PropTypes.bool,
 };
 
 export default Text;
--- src/components/organisms/ParentWrapper/index.js
+++ src/components/organisms/ParentWrapper/index.js
@@ -8,86 +8,102 @@ import axios from 'axios';
 import { API_ENDPOINTS } from '../../../api';
 import { useHistory } from 'react-router-dom';
 import { ENV_CONFIG } from '../../../config';
+import Logo from '../../atoms/Logo/Logo';
+import Text from '../../atoms/Text/Text';
+
 const { Content } = Layout;
 const { Footer } = Layout;
-const { Text } = Typography;
 
 const ParentWrapper = (props) => {
-	const history = useHistory();
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [showBidBlock, setShowBidBlock] = useState(false);
-	const onSelectionChange = (value) => {
-		setShowBidBlock(value);
-		console.log(showBidBlock);
-	};
-	useEffect(() => {
-		if (!localStorage.getItem('attendeeId')) {
-			// let userexternalid = window.location.href //will update once patched to Aventri
-			let userexternalid = '119287966';
-			localStorage.setItem('attendeeId', userexternalid);
-		}
-		let userData = localStorage.getItem('USER');
-		if (
-			userData &&
-			Object.keys(userData).length &&
-			userData.userId &&
-			userData.passphrase
-		) {
-			axios
-				.post(`${ENV_CONFIG.BASE_URL}${API_ENDPOINTS.VERIFY_USER}`, {
-					userId: userData.userId,
-					passsphrase: userData.passphrase,
-				})
-				.then(({ data }) => {
-					if (data && data.data) {
-						setIsLoggedIn(true);
-						localStorage.setItem('USER', JSON.stringify(data.data));
-					}
-				});
-		} else {
-			axios
-				.get(
-					`${ENV_CONFIG.BASE_URL}${
-						API_ENDPOINTS.INITIALIZE_USER
-					}?userexternalid=${localStorage.getItem('attendeeId')}`,
-				)
-				.then(({ data }) => {
-					if (data && data.data && Object.keys(data.data).length) {
-						if (data.data.isActive) {
-							setIsLoggedIn(true);
-						} else history.push('/login');
-					}
-					localStorage.setItem('USER', JSON.stringify(data.data));
-				})
-				.catch((e) => alert('Something went wrong!'));
-		}
-	}, []);
-	return (
-		<>
-			{!isLoggedIn ? (
-				<Text>Hold tight while we setup the platform....</Text>
-			) : (
-				<Layout style={{ height: '100vh' }} id='auction'>
-					<Header />
-					<Content style={{ height: 'calc(100% - 209px)' }}>
-						<Art />
-						<div className={classes.liveChatWrapper}>
-							<LiveChat />
-							<div className={`${showBidBlock ? 'show' : ''} transition-hide`}>
-								<Bid onClose={() => onSelectionChange(false)} />
-							</div>
-						</div>
-					</Content>
-					<Footer
-						className={`${classes.footerStyle} ${classes.footerPosition}`}
-						onClick={() => onSelectionChange(!showBidBlock)}
-					>
-						{/* <Foot openBidBlock={()=>onSelectionChange(!showBidBlock)}/> */}
-						PLACE BID
-					</Footer>
-				</Layout>
-			)}
-		</>
-	);
+  const history = useHistory();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showBidBlock, setShowBidBlock] = useState(false);
+  const onSelectionChange = (value) => {
+    setShowBidBlock(value);
+    console.log(showBidBlock);
+  };
+  useEffect(() => {
+    if (!localStorage.getItem('attendeeId')) {
+      // let userexternalid = window.location.href //will update once patched to Aventri
+      let userexternalid = '119287966';
+      localStorage.setItem('attendeeId', userexternalid);
+    }
+    let userData = localStorage.getItem('USER');
+    if (
+      userData &&
+      Object.keys(userData).length &&
+      userData.userId &&
+      userData.passphrase
+    ) {
+      axios
+        .post(`${ENV_CONFIG.BASE_URL}${API_ENDPOINTS.VERIFY_USER}`, {
+          userId: userData.userId,
+          passsphrase: userData.passphrase,
+        })
+        .then(({ data }) => {
+          if (data && data.data) {
+            setIsLoggedIn(true);
+            localStorage.setItem('USER', JSON.stringify(data.data));
+          }
+        });
+    } else {
+      axios
+        .get(
+          `${ENV_CONFIG.BASE_URL}${
+            API_ENDPOINTS.INITIALIZE_USER
+          }?userexternalid=${localStorage.getItem('attendeeId')}`
+        )
+        .then(({ data }) => {
+          if (data && data.data && Object.keys(data.data).length) {
+            if (data.data.isActive) {
+              setIsLoggedIn(true);
+            } else history.push('/login');
+          }
+          localStorage.setItem('USER', JSON.stringify(data.data));
+        })
+        .catch((e) => alert('Something went wrong!'));
+    }
+  }, []);
+  return (
+    <>
+      {!isLoggedIn ? (
+        <div
+          style={{
+            height: '100vh',
+            fontSize: '14px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Logo width={150} height={'auto'} />
+          <Text noMargin size={'md'} spacing={'md'} primaryColor>
+            Hold tight while we setup the platform....
+          </Text>
+        </div>
+      ) : (
+        <Layout style={{ height: '100vh' }} id='auction'>
+          <Header />
+          <Content style={{ height: 'calc(100% - 209px)' }}>
+            <Art />
+            <div className={classes.liveChatWrapper}>
+              <LiveChat />
+              <div className={`${showBidBlock ? 'show' : ''} transition-hide`}>
+                <Bid onClose={() => onSelectionChange(false)} />
+              </div>
+            </div>
+          </Content>
+          <Footer
+            className={`${classes.footerStyle} ${classes.footerPosition}`}
+            onClick={() => onSelectionChange(!showBidBlock)}
+          >
+            {/* <Foot openBidBlock={()=>onSelectionChange(!showBidBlock)}/> */}
+            PLACE BID
+          </Footer>
+        </Layout>
+      )}
+    </>
+  );
 };
 export default ParentWrapper;
