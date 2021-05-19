3794da8b4a4aa9cb10353aff36b412fcb77189d6
kajal-zeb <kajal.d@zebpay.com>
2021-05-19 09:04:23 +0530

    add username field

---
+++ src/api/index.js
@@ -0,0 +1,6 @@
+export const API_ENDPOINTS = {
+	INITIALIZE_USER: '/api/user/initialise',
+	VERIFY_CODE: '/api/user/verifyCode',
+	VERIFY_USER: '/api/user/verifyUser',
+	PLACE_BID: '/api/user/placeBid',
+};
--- src/components/organisms/Login/Login.js
+++ src/components/organisms/Login/Login.js
@@ -5,43 +5,51 @@ import Sprite from '../../atoms/Sprite/Sprite';
 import classes from './Login.module.scss';
 
 const Login = () => {
-  const [form] = Form.useForm();
-  const onFinish = (data) => {
-    console.log(data);
-  };
+	const [form] = Form.useForm();
+	const onFinish = (data) => {
+		console.log(data);
+	};
 
-  return (
-    <div className={`${classes.loginView}`}>
-      <Logo width={150} height={'auto'} />
-      <div className={`${classes.inputArea}`}>
-        <Form
-          initialValues={{
-            code: '',
-            remember: true,
-          }}
-          name='login'
-          form={form}
-          onFinish={onFinish}
-        >
-          <Form.Item name={'code'}>
-            <Input
-              size={'large'}
-              className={`${classes.inputField}`}
-              placeHolder={'ENTER CODE'}
-            />
-          </Form.Item>
-          <Form.Item className={'mt10'}>
-            <Button
-              htmlType='submit'
-              className={`${classes.inputField} ${classes.button}`}
-            >
-              SUBMIT
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`${classes.loginView}`}>
+			<Logo width={150} height={'auto'} />
+			<div className={`${classes.inputArea}`}>
+				<Form
+					initialValues={{
+            username: '',
+						code: '',
+						remember: true,
+					}}
+					name='login'
+					form={form}
+					onFinish={onFinish}
+				>
+					<Form.Item name={'username'}>
+						<Input
+							size={'large'}
+							className={`${classes.inputField}`}
+							placeHolder={'ENTER CRYPTO NAME'}
+						/>
+					</Form.Item>
+					<Form.Item name={'code'}>
+						<Input
+							size={'large'}
+							className={`${classes.inputField}`}
+							placeHolder={'ENTER CODE'}
+						/>
+					</Form.Item>
+					<Form.Item className={'mt10'}>
+						<Button
+							htmlType='submit'
+							className={`${classes.inputField} ${classes.button}`}
+						>
+							SUBMIT
+						</Button>
+					</Form.Item>
+				</Form>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
