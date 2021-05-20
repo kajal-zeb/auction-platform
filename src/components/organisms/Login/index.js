fa2ccdc9fc66d5f3a4f022501940f8e9e322a3de
karan-parsnani-zeb <karan.p@zebpay.com>
2021-05-20 11:32:46 +0530

    adding message before event starts

--- src/components/organisms/Login/index.js
+++ src/components/organisms/Login/index.js
@@ -1,4 +1,4 @@
-import { Input, Form, Button, Typography } from 'antd';
+import { Input, Form, Button } from 'antd';
 import React, { useEffect, useState } from 'react';
 import Logo from '../../atoms/Logo/Logo';
 import Sprite from '../../atoms/Sprite/Sprite';
@@ -7,76 +7,75 @@ import axios from 'axios';
 import { API_ENDPOINTS } from '../../../api';
 import { useHistory } from 'react-router-dom';
 import ENV_CONFIG from '../../../config';
-const { Text } = Typography;
+import Text from '../../atoms/Text/Text';
 
 const Login = () => {
-	const [form] = Form.useForm();
-	const history = useHistory();
+  const [form] = Form.useForm();
+  const history = useHistory();
 
-	// const patchExternalID = () => {
-	// 	// let data = window.location.href // extract the id from here
-	// 	localStorage.setItem('userexternalid',1) // temp until we get it from url
-	// }
+  // const patchExternalID = () => {
+  // 	// let data = window.location.href // extract the id from here
+  // 	localStorage.setItem('userexternalid',1) // temp until we get it from url
+  // }
 
-	const onFinish = async (data) => {
-		// if(!localStorage.getItem('userexternalid')) {
-		// 	patchExternalID()
-		// }
-		let body = {
-			attendeeId: localStorage.getItem('attendeeId'),
-			code: data.code,
-		};
-		await axios
-			.post(`${ENV_CONFIG.BASE_URL}${API_ENDPOINTS.VERIFY_CODE}`, body)
-			.then(({ data }) => {
-				if (data && data.data && Object.keys(data.data).length !== 0) {
-					localStorage.setItem('USER', JSON.stringify(data.data));
-					history.push('/');
-				} else {
-					alert('Failed to verify code. ');
-				}
-			})
-			.catch((err) => {
-				console.log('Verify code Err : ', err);
-			});
-	};
+  const onFinish = async (data) => {
+    // if(!localStorage.getItem('userexternalid')) {
+    // 	patchExternalID()
+    // }
+    let body = {
+      attendeeId: localStorage.getItem('attendeeId'),
+      code: data.code,
+    };
+    await axios
+      .post(`${ENV_CONFIG.BASE_URL}${API_ENDPOINTS.VERIFY_CODE}`, body)
+      .then(({ data }) => {
+        if (data && data.data && Object.keys(data.data).length !== 0) {
+          localStorage.setItem('USER', JSON.stringify(data.data));
+          history.push('/');
+        } else {
+          alert('Failed to verify code. ');
+        }
+      })
+      .catch((err) => {
+        console.log('Verify code Err : ', err);
+      });
+  };
 
-	return (
-		<div className={`${classes.loginView}`}>
-			<Logo width={150} height={'auto'} />
-			<div className={`${classes.inputArea}`}>
-				<Text>
-					A verification code has been sent to your email. Please enter the code
-					below to continue...
-				</Text>
-				<Form
-					initialValues={{
-						code: '',
-						remember: true,
-					}}
-					name='login'
-					form={form}
-					onFinish={onFinish}
-				>
-					<Form.Item name={'code'}>
-						<Input
-							size={'large'}
-							className={`${classes.inputField}`}
-							placeHolder={'ENTER CODE'}
-						/>
-					</Form.Item>
-					<Form.Item className={'mt10'}>
-						<Button
-							htmlType='submit'
-							className={`${classes.inputField} ${classes.button}`}
-						>
-							SUBMIT
-						</Button>
-					</Form.Item>
-				</Form>
-			</div>
-		</div>
-	);
+  return (
+    <div className={`${classes.loginView}`}>
+      <Logo width={150} height={'auto'} />
+      <div className={`${classes.inputArea}`}>
+        <Text noMargin size={'md'} spacing={'md'} primaryColor>
+          The event has not started yet!
+        </Text>
+        <Form
+          initialValues={{
+            code: '',
+            remember: true,
+          }}
+          name='login'
+          form={form}
+          onFinish={onFinish}
+        >
+          <Form.Item name={'code'}>
+            <Input
+              size={'large'}
+              className={`${classes.inputField}`}
+              placeHolder={'ENTER CODE'}
+            />
+          </Form.Item>
+          <Form.Item className={'mt10'}>
+            <Button
+              htmlType='submit'
+              className={`${classes.inputField} ${classes.button}`}
+            >
+              SUBMIT
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/organisms/ParentWrapper/index.js
+++ src/components/organisms/ParentWrapper/index.js
@@ -17,106 +17,169 @@ const { Content } = Layout;
 const { Footer } = Layout;
 
 const ParentWrapper = (props) => {
-	const history = useHistory();
-	const [errorMsg, showErrorMsg] = useState(false);
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
-	const [showBidBlock, setShowBidBlock] = useState(false);
-	const onSelectionChange = (value) => {
-		setShowBidBlock(value);
-		console.log(showBidBlock);
-	};
-	useEffect(() => {
-		if (!localStorage.getItem('attendeeId')) {
-			// let userexternalid = window.location.href //will update once patched to Aventri
-			let userexternalid = '119287966';
-			localStorage.setItem('attendeeId', userexternalid);
-		}
-		let userData = JSON.parse(localStorage.getItem('USER'));
-		if (
-			userData &&
-			Object.keys(userData).length &&
-			userData.id &&
-			userData.passphrase
-		) {
-			axios
-				.post(`${ENV_CONFIG.BASE_URL}${API_ENDPOINTS.VERIFY_USER}`, {
-					userId: userData.id,
-					passphrase: userData.passphrase,
-				})
-				.then(({ data }) => {
-					if (data && data.data) {
-						setIsLoggedIn(true);
-						localStorage.setItem('USER', JSON.stringify(data.data));
-					}
-				});
-		} else {
-			axios
-				.get(
-					`${ENV_CONFIG.BASE_URL}${
-						API_ENDPOINTS.INITIALIZE_USER
-					}?userexternalid=${localStorage.getItem('attendeeId')}`,
-				)
-				.then(({ data }) => {
-					if (data && data.data && Object.keys(data.data).length) {
-						if (data.data.isActive) {
-							setIsLoggedIn(true);
-						} else history.push('/login');
-						localStorage.setItem('USER', JSON.stringify(data.data));
-						showErrorMsg(false);
-					}
-					showErrorMsg(false);
-				})
-				.catch((e) => {
-					notify('Uh Oh! Something went wrong.');
-					showErrorMsg(true);
-				});
-		}
-	}, []);
+  const history = useHistory();
+  const [errorMsg, showErrorMsg] = useState(false);
+  const [eventStarted, setEventStarted] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [showBidBlock, setShowBidBlock] = useState(false);
+  const onSelectionChange = (value) => {
+    setShowBidBlock(value);
+    console.log(showBidBlock);
+  };
+  useEffect(() => {
+    if (!localStorage.getItem('attendeeId')) {
+      // let userexternalid = window.location.href //will update once patched to Aventri
+      let userexternalid = '119287966';
+      localStorage.setItem('attendeeId', userexternalid);
+    }
+    let userData = JSON.parse(localStorage.getItem('USER'));
+    if (
+      userData &&
+      Object.keys(userData).length &&
+      userData.id &&
+      userData.passphrase
+    ) {
+      axios
+        .post(`${ENV_CONFIG.BASE_URL}${API_ENDPOINTS.VERIFY_USER}`, {
+          userId: userData.id,
+          passphrase: userData.passphrase,
+        })
+        .then(({ data }) => {
+          if (data && data.data) {
+            setIsLoggedIn(true);
+            localStorage.setItem('USER', JSON.stringify(data.data));
+          }
+        });
+    } else {
+      axios
+        .get(
+          `${ENV_CONFIG.BASE_URL}${
+            API_ENDPOINTS.INITIALIZE_USER
+          }?userexternalid=${localStorage.getItem('attendeeId')}`
+        )
+        .then(({ data }) => {
+          if (data && data.data && Object.keys(data.data).length) {
+            if (data.data.isActive) {
+              setIsLoggedIn(true);
+            } else history.push('/login');
+            localStorage.setItem('USER', JSON.stringify(data.data));
+            showErrorMsg(false);
+          }
+          showErrorMsg(false);
+        })
+        .catch((e) => {
+          notify('Uh Oh! Something went wrong.');
+          showErrorMsg(true);
+        });
+    }
+  }, []);
 
-	const notify = (message) => toast.warning(message);
+  useEffect(() => {
+    if (localStorage.getItem('USER')['eventStartTime']) {
+      const interval = setInterval(() => {
+        if (
+          getDateFormat(new Date(), true) ===
+          getDateFormat(
+            new Date(localStorage.getItem('USER')['eventStartTime']),
+            true
+          )
+        ) {
+          setEventStarted(true);
+          clearInterval(interval);
+        }
+      }, 1000);
+      return () => {
+        clearInterval(interval);
+      };
+    }
+  }, [localStorage.getItem('USER')['eventStartTime']]);
 
-	return (
-		<>
-			{!isLoggedIn ? (
-				<div
-					style={{
-						height: '100vh',
-						fontSize: '14px',
-						display: 'flex',
-						flexDirection: 'column',
-						alignItems: 'center',
-						justifyContent: 'center',
-					}}
-				>
-					<Logo width={150} height={'auto'} />
-					<Text noMargin size={'md'} spacing={'md'} primaryColor>
-						{!errorMsg
-							? 'Hold tight while we setup the platform....'
-							: 'Something went wrong. Try refreshing!'}
-					</Text>
-				</div>
-			) : (
-				<Layout style={{ height: '100vh' }} id='auction'>
-					<Header />
-					<Content style={{ height: 'calc(100% - 209px)' }}>
-						<Art />
-						<div className={classes.liveChatWrapper}>
-							<LiveChat />
-							<div className={`${showBidBlock ? 'show' : ''} transition-hide`}>
-								<Bid onClose={() => onSelectionChange(false)} />
-							</div>
-						</div>
-					</Content>
-					<Footer
-						className={`${classes.footerStyle} ${classes.footerPosition}`}
-						onClick={() => onSelectionChange(!showBidBlock)}
-					>
-						{/* <Foot openBidBlock={()=>onSelectionChange(!showBidBlock)}/> */}
-						PLACE BID
-					</Footer>
-				</Layout>
-			)}
-		</>
-	);
+  const getDateFormat = (date, time = false) => {
+    const inputDate = new Date(date);
+    let month = String(inputDate?.getMonth() + 1);
+    let day = String(inputDate?.getDate());
+    const year = String(inputDate?.getFullYear());
+    let hours = '';
+    let minutes = '';
+    let seconds = '';
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    if (time) {
+      hours = String(inputDate?.getHours());
+      minutes = String(inputDate?.getMinutes());
+      seconds = String(inputDate?.getSeconds());
+      if (hours.length < 2) hours = '0' + hours;
+      if (minutes.length < 2) minutes = '0' + minutes;
+      if (seconds.length < 2) seconds = '0' + seconds;
+    }
+
+    return `${year}/${month}/${day}${
+      time ? ` ${hours}:${minutes}:${seconds}` : ``
+    }`;
+  };
+
+  const notify = (message) => toast.warning(message);
+
+  return (
+    <>
+      {!isLoggedIn ? (
+        <div
+          style={{
+            height: '100vh',
+            fontSize: '14px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Logo width={150} height={'auto'} />
+          <Text noMargin size={'md'} spacing={'md'} primaryColor>
+            {!errorMsg
+              ? 'Hold tight while we setup the platform....'
+              : 'Something went wrong. Try refreshing!'}
+          </Text>
+        </div>
+      ) : eventStarted ? (
+        <Layout style={{ height: '100vh' }} id='auction'>
+          <Header />
+          <Content style={{ height: 'calc(100% - 209px)' }}>
+            <Art />
+            <div className={classes.liveChatWrapper}>
+              <LiveChat />
+              <div className={`${showBidBlock ? 'show' : ''} transition-hide`}>
+                <Bid onClose={() => onSelectionChange(false)} />
+              </div>
+            </div>
+          </Content>
+          <Footer
+            className={`${classes.footerStyle} ${classes.footerPosition}`}
+            onClick={() => onSelectionChange(!showBidBlock)}
+          >
+            {/* <Foot openBidBlock={()=>onSelectionChange(!showBidBlock)}/> */}
+            PLACE BID
+          </Footer>
+        </Layout>
+      ) : (
+        <div
+          style={{
+            height: '100vh',
+            fontSize: '14px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Logo width={150} height={'auto'} />
+          <Text noMargin size={'md'} spacing={'md'} primaryColor>
+            The event has not started yet!
+          </Text>
+        </div>
+      )}
+    </>
+  );
 };
 export default ParentWrapper;
